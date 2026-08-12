525fabb9194216619c6b5fc01b702f420d3a2d93
MOHD ABDULLAH <121691856+iamabdullahzubair@users.noreply.github.com>
2026-08-12 14:29:05 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        signature = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            if key in signature:
+                signature[key].append(word)
+            else:
+                signature[key] = [word]
+        ans = []
+        for key, value in signature.items():
+            ans.append(value)
+        return ans
